1c930c1562746db3a572077829b1190cffc328a0
Anyadaniel <anyadaniel1@gmail.com>
2025-01-11 03:22:15 -0600

    new 2025 volunteer, attend and exhibit

--- src/app/attend/page.jsx
+++ src/app/attend/page.jsx
@@ -181,6 +181,25 @@ const Attend = () => {
       General Attendee
     </label>
   </div>
+  
+  <div className="flex items-center mt-3 mb-4">
+    <input
+      id="checkbox-3"
+      type="radio"
+      value="3"
+      name="regType"
+      required
+      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+      checked={formData.regType == "3"}
+      onChange={handleChange}
+    />
+    <label
+      htmlFor="checkbox-3"
+      className="ms-2 text-sm font-medium text-gray-900"
+    >
+      Investor
+    </label>
+  </div>
 
   <div className="flex items-center mb-4">
     <input
